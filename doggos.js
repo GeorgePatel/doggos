4fee359abb5cbc91dfaff8ba5e73f7fa27f0ad72
GeorgePatel <gpatel1126@gmail.com>
2022-06-24 15:31:53 -0400

    Exprimenting with CSS

--- doggos.js
+++ doggos.js
@@ -1,22 +1,45 @@
+// const DOG_URL = "https://dog.ceo/api/breeds/image/random";
+//
+// const doggos = document.querySelector(".doggos");
+//
+// function addNewDoggo() {
+//     const promise = fetch(DOG_URL);
+//
+//     promise
+//         .then(function(response) {
+//             const processingPromise = response.json();
+//             return processingPromise;
+//     })
+//     .then(function(processedResponse) {
+//         // const img = document.createElement("img");
+//         const img = document.querySelector(".loading");
+//         // img.className("loading");
+//         img.src = processedResponse.message;
+//         img.alt = "Cute doggo";
+//         doggos.appendChild(img);
+//     });
+//
+// }
+//
+// document.querySelector(".add-doggo").addEventListener("click", addNewDoggo);
+
 const DOG_URL = "https://dog.ceo/api/breeds/image/random";
 
 const doggos = document.querySelector(".doggos");
 
 function addNewDoggo() {
     const promise = fetch(DOG_URL);
-
     promise
         .then(function(response) {
             const processingPromise = response.json();
             return processingPromise;
-    })
-    .then(function(processedResponse) {
-        const img = document.createElement("img");
-        img.src = processedResponse.message;
-        img.alt = "Cute doggo";
-        doggos.appendChild(img);
-    });
-
+        })
+        .then(function(processedResponse) {
+            const img = document.createElement("img");
+            img.src = processedResponse.message;
+            img.alt = "Cute doggo";
+            doggos.appendChild(img);
+        });
 }
 
 document.querySelector(".add-doggo").addEventListener("click", addNewDoggo);

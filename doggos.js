4c16bccbf9204b6a6f0940309b0e8d9b3daf2505
GeorgePatel <gpatel1126@gmail.com>
2022-06-25 12:54:23 -0400

    Shows loading gif while waiting for AJAX. Added filter options to view certain breeds.

--- doggos.js
+++ doggos.js
@@ -22,12 +22,23 @@
 // }
 //
 // document.querySelector(".add-doggo").addEventListener("click", addNewDoggo);
-
-const DOG_URL = "https://dog.ceo/api/breeds/image/random";
-
 const doggos = document.querySelector(".doggos");
 
+const breed = document.querySelector(".breed");
+
+const pic_length = 300;
+
 function addNewDoggo() {
+    const DOG_URL = "https://dog.ceo/api/breed/" + breed.value + "/images/random";
+
+    const loading = document.createElement("img");
+
+    loading.src = "https://media3.giphy.com/media/3o7bu3XilJ5BOiSGic/giphy.gif";
+    loading.width = pic_length;
+    loading.height = pic_length;
+
+    doggos.appendChild(loading);
+
     const promise = fetch(DOG_URL);
     promise
         .then(function(response) {
@@ -35,10 +46,16 @@ function addNewDoggo() {
             return processingPromise;
         })
         .then(function(processedResponse) {
-            const img = document.createElement("img");
-            img.src = processedResponse.message;
-            img.alt = "Cute doggo";
-            doggos.appendChild(img);
+            const dog = document.createElement("img");
+
+            dog.src = processedResponse.message;
+            dog.width = pic_length;
+            dog.height = pic_length;
+            dog.alt = "Cute doggo";
+            dog.className = "dog";
+
+            doggos.removeChild(loading);
+            doggos.appendChild(dog);
         });
 }
 
